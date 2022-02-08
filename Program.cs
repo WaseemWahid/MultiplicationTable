01a9fc6c9a8a2259299b20f11c0a2c45ceb8fc24
Waseem Wahid <wahid.waseem@gmail.com>
2022-02-07 17:17:52 -0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    class Program
+    { 
+        static void Main()
+        {
+            // Create a 10 by 10 rectangular array
+            int[,] multTable = new int[10,10];
+            // Display the table header
+            Console.WriteLine("Multiplication table of 1 through 10");
+            // Traverse the rows in the table
+            for(int row = 0; row < 10; row++)
+                // Traverse the colums i nthe table
+                for (int column = 0; column < 10; column++)
+                {
+                    // Set Element to row+1 times column+1
+                    multTable[row, column] = (row + 1) * (column + 1);
+                    // DIsplay value after computing it
+                    Console.Write(multTable[row, column]);
+                    //  if not last column, issue a tab
+                    if (column < 9) Console.Write("\t");
+                    // Other wise send cursor to next line
+                    else Console.WriteLine();
+                }
+        }
+    }
+}
